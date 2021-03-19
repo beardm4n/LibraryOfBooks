1c367f41946be53fd0dc01e463421f9c36a85248
beardm4n <dmitriykorovko@gmail.com>
2021-03-19 18:56:26 +0300

    updated book card

--- src/app/shared/book-card/book-card.component.ts
+++ src/app/shared/book-card/book-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-book-card',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-card.component.scss']
 })
 export class BookCardComponent implements OnInit {
+  @Input() data: Book;
+
+  public rating: number[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.fillArrayForRating();
+  }
+
+  fillArrayForRating(): void {
+    for (let i = 0; i < this.data.rating; i++) {
+      this.rating.push(1);
+    }
   }
 
 }
